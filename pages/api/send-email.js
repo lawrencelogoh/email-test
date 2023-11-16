3790f10e1db851b0b79e9b448be325edddfaf435
Lawrence Logoh <lawrence.logoh@gmail.com>
2023-11-16 14:04:18 +0000

    Now using promises in send-email

--- pages/api/send-email.js
+++ pages/api/send-email.js
@@ -3,27 +3,38 @@ import nodemailer from "nodemailer";
 
 export default async function handler(req, res) {
   
-    const message = "Heyo"
+  const message = "Heyo"
 
-    const transporter = nodemailer.createTransport({
-      host: "smtp.gmail.com",
-      port: 587,
+  const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
       auth: {
 	user: process.env.GMAIL_ACCOUNT,
 	pass: process.env.GMAIL_PASSWORD,
       },
-    });
-    // TODO: Get list of admins and send to them
-    transporter
-      .sendMail({
-	from: `Blue Torus <${process.env.GMAIL_ACCOUNT}>`, // sender address
+  });
+
+  const mailData = {
+    	from: `Blue Torus <${process.env.GMAIL_ACCOUNT}>`, // sender address
 	to: 'lawrence@torus.blue', // list of receivers
 	subject: `Nodemailer email test`, // Subject line
 	html: message, // html body
-      })
-      .then((info) => {
-	console.log({ info });
-      });
+  }
+    
+  await new Promise((resolve, reject) => {
+    // send mail
+    
+    transporter.sendMail(mailData, (err, info) => {
+      if (err) {
+        console.error(err);
+        reject(err);
+      } else {
+        console.log(info);
+        resolve(info);
+      }
+    });
+  });
+    
     res.status(200).json({ message: "Heh"})
-  
-}
+    
+  }
